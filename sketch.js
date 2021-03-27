7f7f3695a886ac26540cbbb851cea490710fd015
AnyuthaAbishua <anyuthaabishua1205@gmail.com>
2021-03-27 17:52:58 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-var engine, world;
+var engine, world; 
 var box1, pig1;
 var backgroundImg,platform;
+var Clog;
+var chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +36,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    Clog = new Log (200,300,100,PI/2);
 
     bird = new Bird(100,100);
+
+    chain1 = new Chain (bird.body,Clog.body);
 
 }
 
@@ -61,4 +67,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    Clog.display();
+    chain1.display();
 }
